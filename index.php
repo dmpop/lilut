e4ea0df503c0598932dec37834b01463760aca8a
Dmitri Popov <dmpop@linux.com>
2020-08-31 14:29:41 +0200

    Switch to UIKit

--- index.php
+++ index.php
@@ -1,67 +1,71 @@
 <!DOCTYPE html>
 <html lang="en">
-    <!-- Author: Dmitri Popov, dmpop@linux.com
-	 License: GPLv3 https://www.gnu.org/licenses/gpl-3.0.txt --> 
-    <head>
-	<meta charset="utf-8">
+
+<!-- Author: Dmitri Popov, dmpop@linux.com
+	 License: GPLv3 https://www.gnu.org/licenses/gpl-3.0.txt -->
+
+<head>
 	<title>Lilut</title>
+	<meta charset="utf-8">
 	<link rel="shortcut icon" href="favicon.png" />
-	<link rel="stylesheet" href="lit.css">
-	<link href="https://fonts.googleapis.com/css2?family=Nunito" rel="stylesheet">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-    </head>
-    <body>
-	<div class="c">
-	    <?php
-	    if (!file_exists('uploads')) {
-		mkdir('uploads', 0777, true);
-	    }
-	    if (!file_exists('results')) {
-		mkdir('results', 0777, true);
-	    }
-	    ?>
-	    <h1>Lilut</h1>
-	    <hr>
-	    <form action="process.php" method="post" enctype="multipart/form-data">
-		<p>Select JPEG file:</p>
-		<input type="file" name="fileToUpload" id="fileToUpload"><p></p>
-		<p>Select LUT: </p>
-		<select name="lut">
-		    <?php
-		    $files = glob("luts/*");
-		    foreach ($files as $file) {
-			$filename = basename($file);
-			$name = basename($file, ".png");
-			echo "<option value='$filename'>$name</option>"; 
-		    }
-		    ?>
-		</select>
-		<p></p>
-		<input class="btn primary" type="submit" value="Process" name="submit">
-	    </form>
-	    
-	    <p>
-		<details>
-		    <summary>Help</summary>
-		    <p>To learn how to create Hald-CLUT presets for use with Lilut, refer to the <a href="https://gumroad.com/l/linux-photography">Linux Photography</a> book.
-			<p>How to use Lilut</p>
-			<ol>
-			    <li>
-			    Before you start, place prepared Hald-CLUT files in the PNG format into the <i>luts</i> directory.
-			    </li>
-			    <li>
-				Select the desired JPEG file using the <b>Browse</b> button.
-			    </li>
-			<li>
-			    Select the desired Hald-CLUT preset from the <b>Select LUT</b> drop-down list.
-			</li>
-			<li>
-			    Press the <b>Process</b> button.
-			</li>
-			</ol>
-		</details>
-		    </p>
-	    </p>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.5.7/dist/css/uikit.min.css" />
+	<script src="https://cdn.jsdelivr.net/npm/uikit@3.5.7/dist/js/uikit.min.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/uikit@3.5.7/dist/js/uikit-icons.min.js"></script>
+</head>
+
+<body>
+	<div class="uk-container uk-margin-small-top">
+		<div class="uk-card uk-card-primary uk-card-body">
+			<h1 class="uk-heading-line uk-text-center"><span>Lilut</span></h1>
+			<hr>
+			<?php
+			if (!file_exists('uploads')) {
+				mkdir('uploads', 0777, true);
+			}
+			if (!file_exists('results')) {
+				mkdir('results', 0777, true);
+			}
+			?>
+			<form action="process.php" method="post" enctype="multipart/form-data">
+				<p>Select JPEG file:</p>
+				<input class="uk-input" type="file" name="fileToUpload" id="fileToUpload">
+				<p>Select LUT: </p>
+				<select class="uk-select" name="lut">
+					<?php
+					$files = glob("luts/*");
+					foreach ($files as $file) {
+						$filename = basename($file);
+						$name = basename($file, ".png");
+						echo "<option value='$filename'>$name</option>";
+					}
+					?>
+				</select>
+				<input class="uk-button uk-button-primary uk-margin-top" type="submit" value="Process" name="submit">
+				<button class="uk-button uk-button-default uk-margin-top" type="button" uk-toggle="target: #modal">Help</button>
+			</form>
+		</div>
 	</div>
-    </body>
+
+	<div id="modal" uk-modal>
+		<div class="uk-modal-dialog uk-modal-body">
+			<h2 class="uk-modal-title">Help</h2>
+			<ol>
+				<li>
+					Before you start, place prepared Hald-CLUT files in the PNG format into the <i>luts</i> directory.
+				</li>
+				<li>
+					Select the desired JPEG file using the <b>Browse</b> button.
+				</li>
+				<li>
+					Select the desired Hald-CLUT preset from the <b>Select LUT</b> drop-down list.
+				</li>
+				<li>
+					Press the <b>Process</b> button.
+				</li>
+			</ol>
+		</div>
+	</div>
+</body>
+
 </html>
--- process.php
+++ process.php
@@ -1,82 +1,81 @@
 <!DOCTYPE html>
 <html lang="en">
-    <!-- Author: Dmitri Popov, dmpop@linux.com
+<!-- Author: Dmitri Popov, dmpop@linux.com
 	 License: GPLv3 https://www.gnu.org/licenses/gpl-3.0.txt -->
-    
-    <head>
-	<meta charset="utf-8">
-	<title>Lilut</title>
-	<link rel="shortcut icon" href="favicon.png" />
-	<link rel="stylesheet" href="lit.css">
-	<link href="https://fonts.googleapis.com/css2?family=Nunito" rel="stylesheet">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<style>
-	    img {
-	    border-radius: 1em;
-	    max-width: 100%;
-	    display: block;
-	    align-self: center;
-	    }
-	</style>
-    </head>
-    <body>
-	<div class="c">
-	    <h1>Lilut</h1>
-	    <hr>
-	    <?php
-	    $target_dir = "uploads/";
-	    $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-	    $uploadOk = 1;
-	    $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-	    // Check if image file is a actual image or fake image
-	    if(isset($_POST["submit"])) {
-		$lut=$_POST['lut'];
-		echo $lut." ";
-		$check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-		if($check !== false) {
-		    echo "File is an image - " . $check["mime"] . ".";
-		    $uploadOk = 1;
-		} else {
-		    echo "File is not an image.";
-		    $uploadOk = 0;
-		}
-	    }
-	    // Check if file already exists
-	    //if (file_exists($target_file)) {
-	    //   echo "Sorry, file already exists.";
-	    //    $uploadOk = 0;
-	    //}
-	    // Check file size
-	    //if ($_FILES["fileToUpload"]["size"] > 500000) {
-	    //    echo "Sorry, your file is too large.";
-	    //    $uploadOk = 0;
-	    //}
-	    // Allow certain file formats
-	    if($imageFileType != "jpg" && $imageFileType != "jpeg" && $imageFileType != "JPG" && $imageFileType != "JPEG" ) {
-		echo "Only jpg, jpeg, JPG, and JPEG files are allowed. Make sure the file size doesn't exceed the current upload limit.";
-		$uploadOk = 0;
-	    }
-	    // Check if $uploadOk is set to 0 by an error
-	    if ($uploadOk == 0) {
-		echo " The file was not uploaded.";
-		// if everything is ok, try to upload file
-	    } else {
-		if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-		    echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
 
-		    $imagick = new \Imagick('uploads/'.$_FILES["fileToUpload"]["name"]);
-		    $imagickPalette = new \Imagick(realpath("luts/$lut"));
-		    $imagick->haldClutImage($imagickPalette);
-		    $imagick->writeImage("results/".basename($_FILES["fileToUpload"]["name"]));
-		    unlink('uploads/'.$_FILES["fileToUpload"]["name"]);
-		    
-		} else {
-		    echo "Error uploading the file.";
+<head>
+	<title>Lilut</title>
+	<meta charset="utf-8">
+	<link rel="shortcut icon" href="favicon.png" />
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.5.7/dist/css/uikit.min.css" />
+	<script src="https://cdn.jsdelivr.net/npm/uikit@3.5.7/dist/js/uikit.min.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/uikit@3.5.7/dist/js/uikit-icons.min.js"></script>
+	<style>
+		img {
+			border-radius: 1em;
+			max-width: 100%;
+			display: block;
+			align-self: center;
 		}
-	    }
-	    echo '<a download="results/'.$_FILES["fileToUpload"]["name"].'" href="results/'.$_FILES["fileToUpload"]["name"].'" title="Click to download the file"><img alt="Click to download the file" src="results/'.$_FILES["fileToUpload"]["name"].'"></a>';
-	    ?>
-	    <p><a class="btn" href="index.php">Back</a></p>
+	</style>
+</head>
+
+<body>
+	<div class="uk-container uk-margin-small-top">
+		<div class="uk-card uk-card-default uk-card-body">
+			<h1 class="uk-heading-line uk-text-center"><span>Lilut</span></h1>
+			<?php
+			$target_dir = "uploads/";
+			$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+			$uploadOk = 1;
+			$imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+			if (isset($_POST["submit"])) {
+				$lut = $_POST['lut'];
+				echo "<p class='uk-text-center'>Click on the image to download it.</p>";
+				$check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+				if ($check !== false) {
+					$uploadOk = 1;
+				} else {
+					echo "File is not an image.";
+					$uploadOk = 0;
+				}
+			}
+			// Check if file already exists
+			//if (file_exists($target_file)) {
+			//   echo "Sorry, file already exists.";
+			//    $uploadOk = 0;
+			//}
+			// Check file size
+			//if ($_FILES["fileToUpload"]["size"] > 500000) {
+			//    echo "Sorry, your file is too large.";
+			//    $uploadOk = 0;
+			//}
+			// Allow certain file formats
+			if ($imageFileType != "jpg" && $imageFileType != "jpeg" && $imageFileType != "JPG" && $imageFileType != "JPEG") {
+				echo "Only jpg, jpeg, JPG, and JPEG files are allowed. Make sure the file size doesn't exceed the current upload limit.";
+				$uploadOk = 0;
+			}
+			// Check if $uploadOk is set to 0 by an error
+			if ($uploadOk == 0) {
+				echo " The file was not uploaded.";
+				// if everything is ok, try to upload file
+			} else {
+				if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+					$imagick = new \Imagick('uploads/' . $_FILES["fileToUpload"]["name"]);
+					$imagickPalette = new \Imagick(realpath("luts/$lut"));
+					$imagick->haldClutImage($imagickPalette);
+					$imagick->writeImage("results/" . basename($_FILES["fileToUpload"]["name"]));
+					unlink('uploads/' . $_FILES["fileToUpload"]["name"]);
+				} else {
+					echo "Error uploading the file.";
+				}
+			}
+			echo '<a download="results/' . $_FILES["fileToUpload"]["name"] . '" href="results/' . $_FILES["fileToUpload"]["name"] . '" title="Click to download the file"><img alt="Click to download the file" src="results/' . $_FILES["fileToUpload"]["name"] . '"></a>';
+			?>
+			<a class="uk-button uk-button-primary uk-margin-top" href="index.php">Back</a>
+		</div>
 	</div>
-    </body>
+</body>
+
 </html>
