16473947693d7f7dca4381db9bf4bdeb8657819f
Dmitri Popov <dmpop@linux.com>
2020-12-11 14:12:24 +0100

    Add systemd unit. openSUSE container

--- buildah.sh
+++ buildah.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-container=$(buildah from ubuntu:20.04)
-buildah run $container apt update
-buildah run $container apt install -y php-cli php-imagick
+container=$(buildah from opensuse/leap)
+buildah run $container zypper update
+buildah run $container zypper --non-interactive install php-cli php-imagick
 buildah copy $container . /usr/src/lilut/
 buildah config --workingdir /usr/src/lilut $container
 buildah config --port 8000 $container
