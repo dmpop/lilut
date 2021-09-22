785899f52caa2b7184181cd2855e3bc0f0555fd3
Dmitri Popov <dmpop@linux.com>
2021-09-22 17:35:01 +0200

    New logo, SECURITY

--- index.php
+++ index.php
@@ -30,9 +30,9 @@ include('config.php');
 	}
 	?>
 	<div style="text-align: center;">
-		<img style="display: inline; height: 2em; vertical-align: middle;" src="favicon.svg" alt="logo" />
+		<img style="display: inline; height: 2.5em; vertical-align: middle;" src="favicon.svg" alt="logo" />
 		<h1 class="text-center" style="display: inline; margin-left: 0.19em; vertical-align: middle; letter-spacing: 3px; margin-top: 0em; color: #f6a159ff;">LILUT</h1>
-		<p style="color: lightgray; margin-bottom: 1.5em;">💡 Current upload limit is <u><?php echo $upload_mb; ?>MB</u></p>
+		<p style="color: lightgray; margin-bottom: 1.5em;">Current upload limit is <u><?php echo $upload_mb; ?>MB</u></p>
 	</div>
 	<div class="card">
 		<?php
